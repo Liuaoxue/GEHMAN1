81d6642cf67fa73b2014566c98a110bb975db118
Liuaoxue <95681803+Liuaoxue@users.noreply.github.com>
2024-10-02 03:04:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -17,16 +17,16 @@ from utils import *
 
 
 #Hyper-parameters
-d_node=128                     # Node feature dimension
-epoch=args.epochs              # Number of training epochs
-K=args.multihead                # Number of attention heads
+d_node=128                                                      # Node feature dimension
+epoch=args.epochs                                               # Number of training epochs
+K=args.multihead                                                # Number of attention heads
 lambda_1=args.lambda_1
 lambda_2=args.lambda_2
 lambda_3=args.lambda_3
 device = torch.device('cuda:' + args.cuda if torch.cuda.is_available() else 'cpu')  # Set device
 if __name__ == '__main__':
     #  Load the data and construct the graph
-    g,friend_list_index_test,friend_list_index_val=data(d_node,"JK")       # Load graph and index lists
+    g,friend_list_index_test,friend_list_index_val=data(d_node,"JK")        # Load graph and index lists
     g = g.to(device)                                                        # Move graph to the specified device
 
     # Define relationships in the graph
@@ -63,30 +63,30 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load("pth/best_model.pth"))
     model.eval()
 
-   #  Construct negative graphs for contrastive learning
+    #  Construct negative graphs for contrastive learning
     negative_graph = construct_negative_graph(g, 5, ('user', 'friend', 'user')).to(device)
 
-    # 3. Calculating user embeddings
+    #  Calculating user embeddings
     with torch.no_grad():
         pos_score, neg_score, node_emb, contrastive_loss = model(g, negative_graph, node_features, edge_attr,
                                                                  ('user', 'friend', 'user'))
         user_emb = node_emb['user']
     pos_edge_index_2 = []
-    pos_edge_index = g.edges(etype=('user', 'friend', 'user'))                     # Get positive edge indices
+    pos_edge_index = g.edges(etype=('user', 'friend', 'user'))                                                            # Get positive edge indices
     pos_edge_index_2.append(pos_edge_index[0].cpu().detach().numpy())
     pos_edge_index_2.append(pos_edge_index[1].cpu().detach().numpy())
     pos_edge_index_2 = torch.tensor(np.array(pos_edge_index_2)).to(device)
     
-    # 4. Evaluate on the test set
-    neg_edge_index = neg_edge_in(g, 5, ('user', 'friend', 'user'))                 # Generate negative edge indices
-    link_labels = get_link_labels(pos_edge_index_2, neg_edge_index).to(device)     # Get link labels
-    link_logits = model.predict(user_emb, pos_edge_index_2, neg_edge_index)        # Predict link likelihoods
-    loss_cor = F.binary_cross_entropy_with_logits(link_logits, link_labels)        #  Compute loss (optional)
+    # Evaluate on the test set
+    neg_edge_index = neg_edge_in(g, 5, ('user', 'friend', 'user'))                                                        # Generate negative edge indices
+    link_labels = get_link_labels(pos_edge_index_2, neg_edge_index).to(device)                                            # Get link labels
+    link_logits = model.predict(user_emb, pos_edge_index_2, neg_edge_index)                                               # Predict link likelihoods
+    loss_cor = F.binary_cross_entropy_with_logits(link_logits, link_labels)                                                #  Compute loss (optional)
 
-    #Calculating evaluation metrics
+    # Calculating evaluation metrics
     test_auc, ap, top_k, f1_pos, f1_neg, f1_macro = test(user_emb, g, friend_list_index_test)
 
-    #Printing Results
+    # Printing Results
     print("Test AUC:", test_auc)
     print("Test AP:", ap)
     print("Top K Results:", top_k)
@@ -95,7 +95,7 @@ if __name__ == '__main__':
     print("F1 Score (Macro Average):", f1_macro)
 
     # Clean up the memory
-    del negative_graph           # Delete negative graph to free memory
-    torch.cuda.empty_cache()     # Clear CUDA memory
+    del negative_graph                                                  # Delete negative graph to free memory
+    torch.cuda.empty_cache()                                            # Clear CUDA memory
 
 
